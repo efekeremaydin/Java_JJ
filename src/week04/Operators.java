dda6bb027c32f2cc1c995fb0d444053a42f56bc8
efeaydin <efekrmaydn@gmail.com>
2023-11-24 23:06:00 +0000

    week04

---
+++ src/week04/Operators.java
@@ -0,0 +1,16 @@
+package week04;
+
+public class Operators {
+
+    public static void main(String[] args) {
+
+        int x = 12;
+
+        System.out.println(x++ + +x); // 12 + 13 // ***operatore gelince deger degisiyor
+
+        String var1 = "test";
+        String var2 = "test2";
+
+        System.out.println(var1.equals(var1));
+    }
+}
---
+++ src/week04/SwitchPractice.java
@@ -0,0 +1,33 @@
+package week04;
+
+public class SwitchPractice {
+
+    public static void main(String[] args) {
+
+        int age = 6;
+
+        switch (age) {
+            default:
+                System.out.println("not child");
+                break;
+            case 1:
+                System.out.println("newborn");
+                break;
+            case 2:
+                System.out.println("baby");
+                break;
+            case 3:
+                System.out.println("child");
+                break;
+            case 4:
+                System.out.println("child2");
+                break;
+            case 5:
+                System.out.println("child3");
+                break;
+            case 6:
+        }
+
+
+    }
+}
